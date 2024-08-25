e649474798f19e1e4d55b329750350323638a21d
screret <68943070+screret@users.noreply.github.com>
2024-08-25 15:33:39 +0300

    new model logic port

--- src/main/java/com/gregtechceu/gtceu/api/block/MetaMachineBlock.java
+++ src/main/java/com/gregtechceu/gtceu/api/block/MetaMachineBlock.java
@@ -11,7 +11,6 @@ import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.gregtechceu.gtceu.api.machine.feature.*;
 import com.gregtechceu.gtceu.common.data.GTItems;
-import com.gregtechceu.gtceu.utils.GTUtil;
 
 import com.lowdragmc.lowdraglib.client.renderer.IRenderer;
 import com.lowdragmc.lowdraglib.utils.LocalizationUtils;
